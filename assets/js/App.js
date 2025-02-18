bb902e3063b96dbae92bef8d48eb92f5663fde2a
Ryan Santos <114835661+darkboat4@users.noreply.github.com>
2025-02-18 07:47:00 -0300

    Add files via upload

---
+++ assets/js/App.js
@@ -0,0 +1,11 @@
+function bt1(){
+    let map = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.8013601084285!2d-46.67103022386186!3d-23.539645860796316!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce581fa20dc711%3A0xf42103e650c63605!2sRua%20Atibaia%2C%20218%20-%20Pacaembu%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2001235-010!5e0!3m2!1sen!2sbr!4v1739825680364!5m2!1sen!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
+
+    document.getElementById('mapBox').innerHTML = map;
+}
+
+function bt2(){
+    let map = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.1707777342112!2d-46.642957800000005!3d-23.526359300000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce586f3edaf9b1%3A0x8916ae1f64a29f7b!2sSop%C3%A3o%20com%20Carinho!5e0!3m2!1sen!2sbr!4v1739826520627!5m2!1sen!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
+
+    document.getElementById('mapBox').innerHTML = map;
+}
